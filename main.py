19b728493ab76ff9595500b67f7fd2a43972b449
bogoconic1 <tqy8587@gmail.com>
2023-04-19 23:03:11 +0800

    added all languages option for submissions verdict summary

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import streamlit as st
 import streamlit_extras
 
 st.header("ACRateChecker")
-st.write("A tool to find the acceptance rate of contest problems")
+st.write("A tool to find the acceptance rate of contest problems, and summarise verdicts from contest submissions")
 
